d30b3bca14f562f3420bce4ff50fa8883e91c3b1
codigocaballer <josemariacaballerm@gmail.com>
2022-11-21 18:04:52 +0100

    refactor: rplc home ctrl by webconfig

---
+++ src/main/java/tacos/web/WebConfig.java
@@ -0,0 +1,18 @@
+package tacos.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    /**
+     * Controllers that only paint a view
+     *
+     * @param registry
+     */
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+    }
+}

7292e96c6586ce070d01737ddf56f9c483508899
codigocaballer <josemariacaballerm@gmail.com>
2022-11-29 09:34:11 +0100

    feat: spring sec jdbc

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -1,24 +1,59 @@
 package tacos.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    DataSource dataSource;
+
+    public static final String DEF_USERS_BY_USERNAME_QUERY =
+            "select username,password,enabled " +
+                    "from users " +
+                    "where username = ?";
+    public static final String DEF_AUTHORITIES_BY_USERNAME_QUERY =
+            "select username,authority " +
+                    "from authorities " +
+                    "where username = ?";
+    public static final String DEF_GROUP_AUTHORITIES_BY_USERNAME_QUERY =
+            "select g.id, g.group_name, ga.authority " +
+                    "from groups g, group_members gm, group_authorities ga " +
+                    "where gm.username = ? " +
+                    "and g.id = ga.group_id " +
+                    "and g.id = gm.group_id";
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/h2-console/**");
+    }
+
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth
+//                .inMemoryAuthentication()
+//                .withUser("buzz")
+//                .password("{noop}infinity")
+//                .authorities("ROLE_USER")
+//                .and()
+//                .withUser("woody")
+//                .password("{noop}bullseye")
+//                .authorities("ROLE_USER");
+
         auth
-                .inMemoryAuthentication()
-                .withUser("buzz")
-                .password("{noop}infinity")
-                .authorities("ROLE_USER")
-                .and()
-                .withUser("woody")
-                .password("{noop}bullseye")
-                .authorities("ROLE_USER");
+                .jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery(DEF_USERS_BY_USERNAME_QUERY)
+                .authoritiesByUsernameQuery(DEF_AUTHORITIES_BY_USERNAME_QUERY);
     }
 }
